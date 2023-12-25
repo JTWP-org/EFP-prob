27711aa0d0d21aa397d062a87a6f80a66b090f5e
Salted Cracker Jack <87721660+oneSALTycracker@users.noreply.github.com>
2023-12-25 05:22:28 -0500

    Create efp-prob.py

---
+++ efp-prob.py
@@ -0,0 +1,67 @@
+import time
+import os
+import asyncio
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from pavlov import PavlovRCON
+
+
+class MyHandler(FileSystemEventHandler):
+    def __init__(self, rcon_ip, rcon_port, rcon_pass):
+        self.rcon_ip = rcon_ip
+        self.rcon_port = rcon_port
+        self.rcon_pass = rcon_pass
+
+    def on_created(self, event):
+        if event.is_directory:
+            return
+        else:
+            file_path = event.src_path
+            filename = os.path.basename(file_path)
+            filename_without_extension = os.path.splitext(filename)[0]
+            print(f'New file created: {file_path}, filename: {filename_without_extension}')
+            
+            with open(file_path, 'w') as file:
+                file.write('0')
+
+            asyncio.run(send_rcon_command(filename_without_extension, self.rcon_ip, self.rcon_port, self.rcon_pass))
+
+
+async def send_rcon_command(filename, rcon_ip, rcon_port, rcon_pass):
+    pavlov = PavlovRCON(rcon_ip, rcon_port, rcon_pass)
+    data = await pavlov.send(f"GiveCash {filename}")
+    data1 = await pavlov.send(f"disconnect")
+    pavlov.close()
+    
+    
+    print(data)
+
+
+rcon_ip = "127.0.0.1"  # Set server IP for RCON
+rcon_port = ""
+rcon_pass = ""
+
+
+path_to_rcon_settings = '/home/steam/pavlovserver/Pavlov/Saved/Config/RconSettings.txt'
+with open(path_to_rcon_settings, 'r') as file:
+    for line in file:
+        if 'Password=' in line:
+            rcon_pass = line.strip().split('=')[1]
+        elif 'Port=' in line:
+            rcon_port = line.strip().split('=')[1]
+
+
+monitor_path = '/home/steam/pavlovserver/Pavlov/Saved/Config/ModSave/'
+
+
+if __name__ == "__main__":
+    event_handler = MyHandler(rcon_ip, rcon_port, rcon_pass)
+    observer = Observer()
+    observer.schedule(event_handler, monitor_path, recursive=False)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
